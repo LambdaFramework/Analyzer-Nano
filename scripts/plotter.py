0e85de4363863ea88a2b8dc6ed0cc664ef7c45dd
SiewYan <siew.yan.hoh@cern.ch>
2019-10-09 11:53:40 +0200

    fix line

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -51,8 +51,8 @@ else:
 
 cfg = Config(options.dataset)
 ########## PLOTTER SETTINGS ##########
-#NTUPLEDIR   = '/home/shoh/works/Projects/Analysis/LambdaNano/Run2_2016_v4-pilot/';
-NTUPLEDIR = '/home/shoh/works/Projects/Analysis/LambdaNano/dataset16-v26-WH/';
+NTUPLEDIR   = '/home/shoh/works/Projects/Analysis/LambdaNano/pilot/';
+#NTUPLEDIR = '/home/shoh/works/Projects/Analysis/LambdaNano/dataset16-v26-WH/';
 PLOTDIR     = "plots/%s"%options.dataset
 LUMI        = cfg.lumi() #41860. #35800. # pb-1 Inquire via brilcalc lumi --begin 272007 --end 275376 -u /pb #https://twiki.cern.ch/twiki/bin/viewauth/CMS/PdmV2016Analysis
 #data = []
@@ -139,8 +139,9 @@ if __name__ == "__main__":
         for iroot in filelist[itag]['files']:
             if not os.path.exists(NTUPLEDIR+iroot+".root"):
                 print col.FAIL+NTUPLEDIR+iroot+".root does not exit!", col.ENDC
-            check=False
+                check=False
     if not check:
+        print "EXIT"
         exit()
     #####
     
